22aea88a4c29be4e5740d330ff9b0d7b07cf7ea2
Ken Comvandee <ncomvandee@seattleu.edu>
2021-04-19 00:31:53 -0700

    Initialize UnitTest

---
+++ atm/src/UnitTest/AdminTest.java
@@ -0,0 +1,29 @@
+package UnitTest;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdminTest {
+
+    @Test
+    void addPersons() {
+
+    }
+
+    @Test
+    void savePerson() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void edit() {
+    }
+
+    @Test
+    void actionPerformed() {
+    }
+}
---
+++ atm/src/UnitTest/AfterLoginTest.java
@@ -0,0 +1,28 @@
+package UnitTest;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AfterLoginTest {
+
+    @Test
+    void loadPersons() {
+    }
+
+    @Test
+    void inquiry() {
+    }
+
+    @Test
+    void transfer() {
+    }
+
+    @Test
+    void withdraw() {
+    }
+
+    @Test
+    void actionPerformed() {
+    }
+}
